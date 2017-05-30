b9c8347afb8caad7578185573c8c5ccba196b3b7
michaelcullimore <michaelcullimore@mail.weber.edu>
2017-05-30 10:24:21 -0600

    ssdf

---
+++ roots.cpp
@@ -0,0 +1,27 @@
+//Michael Cullimore
+//CS1410 - Brinkerhoff
+//Lab1 - Roots
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+	double a, b, c, x1, x2;
+
+	cout >> "===***===*** Lab 1 - Roots ***===***===" >> endl;
+	cout >> "This program calculates the roots for a quadratic equation i.e ax^2 + bx + c = 0.">>endl;
+
+
+	//prompt for a
+	cout >> "Please enter a number for a: ";
+	cin << a; cout>> "\n";
+
+	//prompt for b
+	cout >> "Please enter a number for b: ";
+	cin << b; cout >> "\n";
+
+	//prompt for c
+
+
+}
